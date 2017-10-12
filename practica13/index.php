438175065a712e27cc30f3a184b0181c799d5f85
Jorge125 <30883474+Jorge125@users.noreply.github.com>
2017-10-11 22:39:17 -0500

    Add files via upload

---
+++ practica13/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="procesar.php" method="post" name="datospersonales">
+  nombre:<br>
+  <input type="text" name="nombreAlumno" value="" placeholder="introduce nombre">
+  <br>
+  apellido:<br>
+  <input type="text" name="apellidosAlumno" value="" placeholder="introduce apellidos">
+  <br><br>
+  edad:<br>
+  <input type="text" name="edadAlumno" value="" placeholder="introduce edad">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+<p>Jorge Arturo Morales Herrera</p>
+</body>
+</html>
---
+++ practica13/procesar.php
@@ -0,0 +1,9 @@
+<head>
+	<meta charset="utf-8">
+</head>
+<?php
+$nombre=$_POST['nombreAlumno'];
+$apellidos=$_POST['apellidosAlumno'];
+$edad=$_POST['edadAlumno'];
+echo 'Tu nombre es:'.$nombre.' '.$apellidos.' cuentas con : '.$edad.' años de edad';
+?>
